129ebaadf37bb227329058fd56bdcff65503ca36
unknown <particular6@proton.me>
2024-03-01 01:43:15 +0500

    Added jwt functionality for the login purposes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 const uri = process.env.URI;
@@ -21,7 +22,7 @@ const reg = async (req, res) => {
         const hash = await bcrypt.hash(password, 10);
         const user = await User.create({ username, email, password: hash});
         
-        res.json({ message: "User created,", user: user });
+        res.json({ message: "User created,", user });
     }   catch(err) {
         console.log('Something is wrong, ', err);
         res.status(400).json({ message: "Something is wrong...", error: err });
@@ -30,4 +31,38 @@ const reg = async (req, res) => {
     }
 }
 
-module.exports = { reg };
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        await mongoose.connect(uri);
+        console.log('connected for db and proceeding to login');
+
+        const emailExists = await User.exists( { email });
+    
+        if (!emailExists) {
+          return res.status(400).send('No user with this email.');
+        }
+
+        const user = await User.findOne( {email} );
+        console.log(user);
+        const hash = user.password;
+        const passwordMatch = await bcrypt.compare(password, hash);
+
+        if (!passwordMatch) {
+            return res.status(400).send('Please enter a valid password.');
+        }
+
+        const secret = process.env.SECRET;
+        const token = jwt.sign({username: user.username, email}, secret, {expiresIn: '24h'});
+        
+        res.json({ message: "Please don't show it to anyone", token });
+    }   catch(err) {
+        console.log('Something is wrong, ', err);
+        res.status(400).json({ message: "Something is wrong...", error: err });
+    }   finally {
+        mongoose.disconnect();
+    }
+}
+
+module.exports = { reg, login };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    reg
+    reg, login
 } = require('../controllers/userController');
 
 router.post('/reg', reg);
+
+router.post('/login', login);
 
 module.exports = router;
 
